63cc7e3480b442109a8e48bc67ed9116641ce6ae
mohamedelaraby <elarabym857@gmail.com>
2022-10-26 23:59:44 +0200

    responsive and open close sidebar

--- src/components/Layout/side-bar/SideBar.jsx
+++ src/components/Layout/side-bar/SideBar.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './side-bar.scss'
 import logo from '../../../assets/images/logo.svg'
+import menu from '../../../assets/images/menu.svg'
+import cancel from '../../../assets/images/cancel-circle.svg'
 import { sidebarItems } from '../../../Data'
 
 
@@ -9,13 +11,58 @@ import { sidebarItems } from '../../../Data'
 function SideBar() {
     // eslint-disable-next-line
     const [selected, setSelected] = useState(false);
+    const [openSidebar, setopenSidebar] = useState(0);
 
+
+    function useWindowDimensions() {
+
+        const hasWindow = typeof window !== 'undefined';
+
+        function getWindowDimensions() {
+            const width = hasWindow ? window.innerWidth : null;
+            const height = hasWindow ? window.innerHeight : null;
+            return {
+                width,
+                height,
+            };
+        }
+
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+        useEffect(() => {
+            if (hasWindow) {
+                function handleResize() {
+                    setWindowDimensions(getWindowDimensions());
+                }
+
+                window.addEventListener('resize', handleResize);
+                return () => window.removeEventListener('resize', handleResize);
+            }
+        }, [hasWindow]);
+
+        return windowDimensions;
+    }
+
+    const { height, width } = useWindowDimensions();
     return (
         <>
-            <div className="sidebar">
+
+            {width < 427 ? (<div className='menu-icon' onClick={() => {
+                setopenSidebar(1)
+            }}>
+                <img src={menu} alt="menu" className='menu-icon-img' />
+            </div>) : (<div></div>)}
+
+
+            <div className={openSidebar === 0 && width < 427 ? "sidebar non-display" : "sidebar"}>
                 <div className="sidebar__container">
                     <div className="sidebar__logo">
                         <img src={logo} alt="logo" className='sidebar__logo__img' />
+                        {width < 427 ? (<div className='sidebar__logo__closeicon' onClick={() => {
+                            setopenSidebar(0)
+                        }}>
+                            <img src={cancel} alt="menu" className='sidebar__logo__closeicon__img' />
+                        </div>) : (<div></div>)}
                     </div>
                     <div className="sidebar__items">
                         <div className="sidebar__items__container">
